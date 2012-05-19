0a11a3f715e8d583c87bcbb64b95dbf33eeee27e
Fabian281087@gmail.com <Fabian281087@gmail.com@8e26a171-caff-bad5-9bf1-a0836f8bb24f>
2012-05-19 09:23:43 +0000

    19 de mayo

--- trunk/apps/frontend/modules/estudiar/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/estudiar/templates/indexSuccess.php
@@ -3,8 +3,7 @@
 <br></br><br></br>
 <div style="text-align: center;">
     
-    
-<?php echo link_to(image_tag('iconos/estudiar.gif','class=imageMenuEstudiar').'<br>Estudiar', 'estudiar/listado', array('title' => 'Estudiar viendo listado de palabras')) ?><br></br>
-<?php echo link_to(image_tag('iconos/prueba.gif','class=imageMenuEstudiar').'<br>Realizar exámen', 'estudiar/prueba', array('title' => 'Realizar prueba de todas las palabras')) ?>
+<?php echo link_to(image_tag('iconos/prueba.gif','class=imageMenuEstudiar').'<br>Realizar exámen', 'estudiar/prueba', array('title' => 'Realizar exámen de todas las palabras')) ?>   <br></br> 
+<?php echo link_to(image_tag('iconos/estudiar.gif','class=imageMenuEstudiar').'<br>Estudiar', 'estudiar/listado', array('title' => 'Estudiar viendo listado de palabras')) ?>
 
 </div>
